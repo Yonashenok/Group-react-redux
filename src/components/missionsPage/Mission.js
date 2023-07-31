191853463d8320206938b9be64dc66cf12515eac
Yonashenok <yonashenok24@gmail.com>
2023-07-31 17:08:57 +0300

    feat: display mission list render

---
+++ src/components/missionsPage/Mission.js
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+
+function Mission() {
+  const { missions } = useSelector((state) => state.miss);
+
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.length >= 0 &&
+            missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+                <td>{mission.description}</td>
+                <td>
+                  <button type="button">Active member</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Mission;
