da46fc2bb48173829d05bb68f93fcd3216979df5
Yonashenok <yonashenok24@gmail.com>
2023-07-31 17:11:26 +0300

    feat: setup route on app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { Routes, Route, Outlet } from 'react-router-dom';
-// import { fetchMission } from './redux/missions/missionSlice';
-// import PageNav from './components/PageNav/PageNav';
-// import MyProfile from './pages/MyProfile';
-// import Missions from './pages/Missions';
-// import Rockets from './pages/Rockets';
-// import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import { fetchMission } from './redux/missions/missionSlice';
+import PageNav from './components/PageNav/PageNav';
+import MyProfile from './pages/MyProfile';
+import Missions from './pages/Missions';
+import Rockets from './pages/Rockets';
+import './App.css';
 
-// function Layout() {
-//   const dispatch = useDispatch();
+function Layout() {
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     dispatch(fetchMission());
-//   }, [dispatch]);
-//   return (
-//     <>
-//       <PageNav />
-//       <Outlet />
-//     </>
-//   );
-// }
+  useEffect(() => {
+    dispatch(fetchMission());
+  }, [dispatch]);
+  return (
+    <>
+      <PageNav />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <h1>home page</h1>
-    </div>
-    // <Routes>
-    //   <Route path="/" element={<Layout />}>
-    //     <Route index element={<h1>home</h1>} />
-    //     <Route path="missions" element={<Missions />} />
-    //     <Route path="rockets" element={<Rockets />} />
-    //     <Route path="myProfile" element={<MyProfile />} />
-    //     <Route path="*" element={<h2>No page found</h2>} />
-    //   </Route>
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>home</h1>} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="rockets" element={<Rockets />} />
+        <Route path="myProfile" element={<MyProfile />} />
+        <Route path="*" element={<h2>No page found</h2>} />
+      </Route>
+    </Routes>
   );
 }
 
