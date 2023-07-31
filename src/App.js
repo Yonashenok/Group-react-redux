42bd4a0516c1589a0359c047c070ad20ec760f80
Yonashenok <yonashenok24@gmail.com>
2023-07-31 16:51:05 +0300

    feat: testing dispaly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Routes, Route, Outlet } from 'react-router-dom';
+// import { fetchMission } from './redux/missions/missionSlice';
+// import PageNav from './components/PageNav/PageNav';
+// import MyProfile from './pages/MyProfile';
+// import Missions from './pages/Missions';
+// import Rockets from './pages/Rockets';
+// import './App.css';
+
+// function Layout() {
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     dispatch(fetchMission());
+//   }, [dispatch]);
+//   return (
+//     <>
+//       <PageNav />
+//       <Outlet />
+//     </>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>home page</h1>
     </div>
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<h1>home</h1>} />
+    //     <Route path="missions" element={<Missions />} />
+    //     <Route path="rockets" element={<Rockets />} />
+    //     <Route path="myProfile" element={<MyProfile />} />
+    //     <Route path="*" element={<h2>No page found</h2>} />
+    //   </Route>
+    // </Routes>
   );
 }
 
