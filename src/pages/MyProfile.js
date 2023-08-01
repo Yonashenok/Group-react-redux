63b9f47b4f4eb5557732885d3fcae4ace1ab4355
Yonashenok <yonashenok24@gmail.com>
2023-08-01 12:29:10 +0300

    feat: add filter on  joined mission and display

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,32 @@
+import { useSelector } from 'react-redux';
+import classes from './MyProfile.module.css';
+
 function MyProfile() {
+  const { missions } = useSelector((state) => state.miss);
+
+  const reservedMission = missions.filter(
+    (mission) => mission.missionStatus === true,
+  );
+
   return (
-    <div>
-      <h1>My Profile</h1>
+    <div className={classes.container}>
+      <div className={classes.containMission}>
+        <h2 className={classes.headText}>My Missions</h2>
+        <ul className={classes.missions}>
+          {reservedMission.map((mission) => (
+            <li key={mission.mission_id}>{mission.mission_name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={classes.containMission}>
+        <h2 className={classes.headText}>My Rockets</h2>
+        <ul className={classes.missions}>
+          <li>Telstar</li>
+          <li>Telstar</li>
+          <li>Telstar</li>
+          <li>Telstar</li>
+        </ul>
+      </div>
     </div>
   );
 }
