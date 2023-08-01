09923e8213dda4fc789889f4403a1feffce891a9
Yonashenok <yonashenok24@gmail.com>
2023-08-01 10:43:54 +0300

    Style: mission page  table

--- src/components/missionsPage/Mission.js
+++ src/components/missionsPage/Mission.js
@@ -1,26 +1,54 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleMission } from '../../redux/missions/missionSlice';
+import classes from './Mission.module.css';
 
 function Mission() {
   const { missions } = useSelector((state) => state.miss);
+  const isMission = missions.length >= 0;
+
+  const dispatch = useDispatch();
 
   return (
-    <div>
-      <table className="table table-striped">
+    <div className={classes.container}>
+      <table className={classes.tableContainer}>
         <thead>
-          <tr>
+          <tr className={classes.theadContainer}>
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
+            <th>{false}</th>
           </tr>
         </thead>
         <tbody>
-          {missions.length >= 0
+          {isMission
             && missions.map((mission) => (
-              <tr key={mission.mission_id}>
-                <td>{mission.mission_name}</td>
-                <td>{mission.description}</td>
+              <tr className={classes.missionDetails} key={mission.mission_id}>
+                <td className={classes.missionTitle}>{mission.mission_name}</td>
+                <td className={classes.tableText}>{mission.description}</td>
                 <td>
-                  <button type="button">Active member</button>
+                  <button
+                    className={
+                      mission.missionStatus
+                        ? classes.activeMember
+                        : classes.memberBtn
+                    }
+                    type="button"
+                  >
+                    {mission.missionStatus ? 'Active Member' : 'NOT A MEMBER'}
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className={
+                      mission.missionStatus
+                        ? classes.leaveMission
+                        : classes.joinBtn
+                    }
+                    type="button"
+                    onClick={() => dispatch(toggleMission(mission.mission_id))}
+                  >
+                    {mission.missionStatus ? 'Leave Mission' : 'Join Mission'}
+                  </button>
                 </td>
               </tr>
             ))}
